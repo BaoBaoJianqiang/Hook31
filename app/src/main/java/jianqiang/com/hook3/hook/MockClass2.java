807e47a1cc26a6319d62e4e1a437aa58d1c1675f
baobaojianqiang <16230091@qq.com>
2018-08-19 03:16:10 +0800

    support api28: hook H

--- app/src/main/java/jianqiang/com/hook3/hook/MockClass2.java
+++ app/src/main/java/jianqiang/com/hook3/hook/MockClass2.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 
+import java.util.List;
+
 import jianqiang.com.hook3.RefInvoke;
 
 /**
@@ -24,8 +26,11 @@ class MockClass2 implements Handler.Callback {
         switch (msg.what) {
             // ActivityThread里面 "LAUNCH_ACTIVITY" 这个字段的值是100
             // 本来使用反射的方式获取最好, 这里为了简便直接使用硬编码
-            case 100:
+            case 100:   //for API 28以下版本
                 handleLaunchActivity(msg);
+                break;
+            case 159:   //for API 28
+                handleActivity(msg);
                 break;
 
         }
@@ -45,5 +50,19 @@ class MockClass2 implements Handler.Callback {
         intent.setComponent(targetIntent.getComponent());
     }
 
+    private void handleActivity(Message msg) {
+        // 这里简单起见,直接取出TargetActivity;
+        Object obj = msg.obj;
 
+        List<Object> mActivityCallbacks = (List<Object>) RefInvoke.getFieldObject(obj, "mActivityCallbacks");
+        if(mActivityCallbacks.size() > 0) {
+            String className = "android.app.servertransaction.LaunchActivityItem";
+            if(mActivityCallbacks.get(0).getClass().getCanonicalName().equals(className)) {
+                Object object = mActivityCallbacks.get(0);
+                Intent intent = (Intent)RefInvoke.getFieldObject(object, "mIntent");
+                Intent targetIntent = intent.getParcelableExtra(AMSHookHelper.EXTRA_TARGET_INTENT);
+                intent.setComponent(targetIntent.getComponent());
+            }
+        }
+    }
 }
